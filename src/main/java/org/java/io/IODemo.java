d5eaf3773b751a5b8bf91d76877e22690002c2ad
java912041430 <Java_912041430@aliyun.com>
2021-02-01 00:12:20 +0800

    添加测试img图片

--- src/main/java/org/java/io/IODemo.java
+++ src/main/java/org/java/io/IODemo.java
@@ -1,5 +1,7 @@
 package org.java.io;
 
+import org.java.util.PrintUtil;
+
 import java.io.*;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
@@ -9,13 +11,15 @@ public class IODemo {
     public static void main(String[] args) throws Exception{
 //        copy();
 //        copyNIO();
-        copyFileUsingJava7Files();
+//        copyFileUsingJava7Files();
+        //复制所有
+        allCopy();
     }
 
     //使用FileStreams复制
     private static void copy() throws IOException {
         File inputFile = new File("C:\\Users\\NAME_CHENZHE\\Pictures\\照片\\001e8c9cb843b0f668fac70f762be6c6e4412077.png");
-        File outputFile = new File("F:\\TestDemo\\Re\\居居.png");
+        File outputFile = new File("src\\img\\123.png");
 
         InputStream inputStream = new FileInputStream(inputFile);
         OutputStream outputStream = new FileOutputStream(outputFile);
@@ -52,7 +56,39 @@ public class IODemo {
         Files.copy(inputFile.toPath(),outputFile.toPath());
     }
 
+    private static void allCopy() throws IOException {
+        final String inputPath = "C:\\Users\\NAME_CHENZHE\\Pictures\\照片\\";
+        final String outputPath = "src\\img\\";
+        File inputFile = new File(inputPath);
+        File outputFile = new File(outputPath);
+        String[] listInfo = inputFile.list();
+        if(listInfo == null){
+            PrintUtil.printInfo("listInfo为空！");
+            return;
+        }
 
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        for (String msg : listInfo){
+            if(msg.contains("jpg")|| msg.contains("png")){
+                File inputFileByMsg = new File(inputPath + "//" + msg);
+                File outputFileByMsg = new File(outputPath + "//" + msg);
+                inputStream = new FileInputStream(inputFileByMsg);
+                outputStream = new FileOutputStream(outputFileByMsg);
+
+                byte[] bytes = new byte[2048];
+                int len;
+                while((len=inputStream.read(bytes))>-1){
+                    outputStream.write(bytes,0,len);
+                }
+            }
+        }
+
+        if(inputStream!=null){
+            inputStream.close();
+            outputStream.close();
+        }
+    }
 
 
 }
